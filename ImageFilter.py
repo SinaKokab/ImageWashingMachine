b0ab33d5dcf59dd84d802e82d9f160d2d6bd90d1
Sina Kokab <sinakokab@gmail.com>
2018-11-03 19:53:39 +0000

    ImageFilter.py
    Functionality - 
            * Resizes images, changing the MD5 hash.
            * Counts number of resized images
            * Adds resized images to a dated folder within parent folder
            * Has an optional image filter mode for processed images. Currently only has a vintage image filter loaded.

---
+++ ImageFilter.py
@@ -0,0 +1,82 @@
+from PIL import Image
+import os, sys, shutil, datetime, vintage
+
+#TODO -
+#Add more image filters
+#Add auto watermark facility (Watermark loadble by path)
+#Multithreading to make saving process faster
+#Add quote facility
+    #Text file of quotes added to the bottom center of the picture
+
+
+path = ("C:/Users/Sina/Desktop/picturestest")
+datestr = datetime.date.today().strftime("%d-%m-%y")
+makephotosvintage = False
+UserInput = ""
+UserChosen = False
+NumberOfPhotos = 0
+NumberOfPhotosResized = 0
+
+if not os.path.exists(path):
+    print("Making directory...")
+    os.mkdir(path)
+
+if not os.path.exists(path + "/resizedimages"):
+    print("Making directory...")
+    os.mkdir(path + "/resizedimages")
+
+if not os.path.exists(path + "/resizedimages/" + datestr):
+    print("Making directory...")
+    os.mkdir(path + "/resizedimages/" + datestr)
+
+NumberOfPhotos =  len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f[0] != '.'])
+
+print("Number of Photos - " + str(NumberOfPhotos))
+
+while (UserChosen == False):
+    UserInput = raw_input("Make photos vintage? Y/N")
+    if (UserInput.lower() == 'y'):
+        makephotosvintage = True
+        UserChosen = True
+    elif (UserInput.lower() == 'n'):
+        makephotosvintage = False
+        UserChosen = True
+    else:
+        print('User\'s input is to be given as (Y/N)\n')
+
+
+def resize():
+    global NumberOfPhotosResized
+    for item in os.listdir(path):
+        #print ("Resized!")
+        #print (item)
+        item = path + "/" + item
+        print(item)
+        if os.path.isfile(item):
+            #print ("Resized! 2")
+            im = Image.open(item)
+            width, height = im.size
+            f, e = os.path.splitext(item)
+            imResize = im.resize((width - 1, height - 1), Image.ANTIALIAS)
+            outputfiledirectory = os.path.join(path + "/resizedimages")
+            if makephotosvintage == True:
+                imResize = vintage.vintage_colors(imResize)
+                #print("Make photo vintage...")
+            imResize.save(f + " resized.jpg", 'JPEG', quality=90) #+ f + ' resized.jpg', 'JPEG', quality=90)
+            NumberOfPhotosResized += 1
+            #print("file - " + f)
+            #f ("resized") in f:
+                #print("Moving file...")
+                #shutil.move((f+".jpg"), path + "/resizedimages")
+                #imResize.
+            for xyz in os.listdir(path):
+                print(xyz)
+                if "resized" in xyz and "resizedimages" not in xyz:
+                    #print("Moving image...")
+                    shutil.move(path + "/" + xyz, path + "/resizedimages/" + datestr + "/" + xyz)
+
+        #return NumberOfPhotosResized
+
+
+resize()
+print ("Number of Photos resized - " + str(NumberOfPhotosResized))
